96829497b84c3f1a2b2fdb140c98740f8f849e59
Aman <amanmaurya8419@gmail.com>
2022-10-11 14:03:15 +0530

    adding Day 4 programs

---
+++ Day 4 programs/problem3.js
@@ -0,0 +1,7 @@
+let char = "b";
+
+if (char != "a" && char != "e" && char != "i" && char != "o" && char != "u"){
+  console.log("Consonent");
+}else {
+  console.log("Not a Consonent");
+}
---
+++ Day 4 programs/problem4.js
@@ -0,0 +1,5 @@
+let a = 4;
+let b = 5;
+let c = 2;
+
+a > b && a > c ? console.log(a, "is Greater") : b > a && b > c ? console.log(b, "is Greater") : console.log(c, "is Greater");
